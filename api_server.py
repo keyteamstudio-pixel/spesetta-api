462a8a2b686b10beae7f7abc52cf791f67328780
keyteamstudio-pixel <key.teamstudio@gmail.com>
2025-11-03 12:30:46 +0100

    Update api_server.py

--- api_server.py
+++ api_server.py
@@ -60,5 +60,5 @@ def search(query):
         return jsonify({"errore": str(e)}), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 3000))
     app.run(host="0.0.0.0", port=port)
